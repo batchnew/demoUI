de1ad11b03c69f62f0b901ec0f2afaf029d77ca2
umesh2997 <umeshpamu9@gmail.com>
2024-04-28 23:31:52 +0530

    loginPage

---
+++ TestAutomationFrameWork/src/test/java/com/constructer/Login.java
@@ -0,0 +1,11 @@
+package com.constructer;
+
+import org.testng.reporters.jq.Main;
+
+public class Login {
+	
+	public static void main(String[] args) {
+		System.out.println("I Love India");
+	}
+
+}
