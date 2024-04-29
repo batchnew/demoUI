c3b57a56226c34164fe144eee962be68c3b56840
umesh2997 <umeshpamu9@gmail.com>
2024-04-29 20:07:48 +0530

    ****

--- TestAutomationFrameWork/src/test/java/com/constructer/CreditPage.java
+++ TestAutomationFrameWork/src/test/java/com/constructer/CreditPage.java
@@ -4,6 +4,7 @@ public class CreditPage {
 	public static void main(String[] args) {
 		System.out.println("How Are You");
 		System.out.println("123");
+		System.out.println("****************");
 	}
 
 }
